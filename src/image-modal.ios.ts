3675e791bfdb924527073a9f044af7426c843553
Erick Arroyo <erick.1726@gmail.com>
2018-04-02 16:38:09 -0500

    Adding ios support

--- src/image-modal.ios.ts
+++ src/image-modal.ios.ts
@@ -1,5 +1,21 @@
-import { Common } from './image-modal.common';
+import {Common} from './image-modal.common';
+
+let window: UIWindow;
+
+declare var ImageModalIOS: any;
+let imageModal;
 
 export class ImageModal extends Common {
+    constructor() {
+        super();
+    }
+
+    public showModal(url: string, cb: any): void {
+        window = UIApplication.sharedApplication.keyWindow;
+        imageModal = ImageModalIOS.alloc().init();
+        imageModal.setFrameWithFrame(window.bounds);
+        cb ? imageModal.setImageWithUrl(url) : imageModal.setImageWithUrlCb(url, cb);
+        window.addSubview(imageModal.getView());
+    }
 
 }
